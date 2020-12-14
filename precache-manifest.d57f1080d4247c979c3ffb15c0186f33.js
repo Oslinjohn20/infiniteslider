2f1906cfcbf9d6a3c872878db56dd29b1c3126f0
Oslinjohn20 <ossiekcjohnson@gmail.com>
2020-12-14 07:57:49 +0200

    Updates

---
+++ precache-manifest.d57f1080d4247c979c3ffb15c0186f33.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b884f814dba3b91dc5f27c964b629297",
+    "url": "/infiniteslider/index.html"
+  },
+  {
+    "revision": "d5a782bc150e4d99cb36",
+    "url": "/infiniteslider/static/css/2.296cf239.chunk.css"
+  },
+  {
+    "revision": "c071385ee2763743f2e8",
+    "url": "/infiniteslider/static/css/main.88b8d1a4.chunk.css"
+  },
+  {
+    "revision": "d5a782bc150e4d99cb36",
+    "url": "/infiniteslider/static/js/2.b27acacc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/infiniteslider/static/js/2.b27acacc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c071385ee2763743f2e8",
+    "url": "/infiniteslider/static/js/main.e36a4bca.chunk.js"
+  },
+  {
+    "revision": "51fad424b65d77857819",
+    "url": "/infiniteslider/static/js/runtime-main.351d17b8.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/infiniteslider/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/infiniteslider/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/infiniteslider/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/infiniteslider/static/media/slick.f97e3bbf.svg"
+  }
+]);
